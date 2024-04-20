b3081d3ae8ff85126b6743b6e66e6344a7f65093
Tamil Iniyan S <tamiliniyans1999@gmail.com>
2024-04-20 16:01:08 +0530

    Completed desktop design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,16 @@
 import styles from "../styles/Hero.module.css";
 
 export const Hero = () => {
-  return <section className={styles.hero}>Hero</section>;
+  return (
+    <section className={styles.hero}>
+      <img src="/logo.svg" alt="Home" className={styles.logo} />
+      <h1 className={styles.title}>We make your music sound extraordinary.</h1>
+      <p className={styles.description}>
+        A system audio equalizer specifically designed for Android and iOS.
+        Freely tune the way your music sounds with a professional grade
+        parametric EQ & volume mixer. Control bass, mids, treble, gain control,
+        reverb, and more!
+      </p>
+    </section>
+  );
 };
